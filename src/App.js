be1a16dc9648c5fc016e03a60f880d38f8383f08
Rudy Flores <38470001+rudypflores@users.noreply.github.com>
2019-02-17 09:02:11 -0600

    final version

--- src/App.js
+++ src/App.js
@@ -9,27 +9,18 @@ class App extends Component {
     	<div>
 	    	<div className="title">
 	    		<p>DragonFlyTracer</p>
+	    		<p className="sub">The tweet location tracker</p>
 	    	</div>
 	    	<div className="google-maps">
 	    		<GeoCode/>
 	    	</div>
 	    	<div className="description">
-	    		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-	    		 Mauris pulvinar mauris nulla, quis vestibulum neque feugiat
-	    		  placerat. Maecenas at facilisis lorem. Etiam ac cursus ligula.
-	    		   Vestibulum sed placerat nulla. Praesent viverra erat in 
-	    		   pretium lacinia. Suspendisse potenti. Nam orci ipsum,
-	    		    feugiat id commodo vel, rutrum in orci.
-
-				Nullam sed lectus sed nulla volutpat ornare eget pulvinar leo. 
-				Sed nec quam vitae sapien volutpat pharetra a in eros. Sed vitae
-				 vestibulum velit, ut porta elit. Maecenas at erat eros. Cras 
-				 euismod mauris ac pulvinar consectetur. Pellentesque non neque 
-				 ligula. Cras vel eleifend purus. Sed iaculis ullamcorper elit et 
-				 accumsan. Sed erat metus, sodales nec pretium non, auctor sit amet
-				  nulla.</p>
+	    		<p>This Web Application was developed with <i>React.js</i>, 
+	    		we provide our Google Maps user location placement feature through
+	    		the <i>Google Maps API</i>. </p>
 	    	</div>
 	    	<div className="contributors">
+	    		<p className="sub-title">- A collaborative project between -</p>
 	    		<p>Rudy Flores | Tommy Bart | Sean | Hunter</p>
 	    	</div>
     	</div>
