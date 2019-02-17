b7f32eafe3b980161a96a85f27f56525d9a547b2
Rudy Flores <38470001+rudypflores@users.noreply.github.com>
2019-02-17 11:00:30 -0600

    Updates

---
+++ precache-manifest.8ee982017dac8637a6650a4ad62c8707.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25ff887ca302a8c7e011",
+    "url": "/DragonFlyTracer/static/js/runtime~main.25ff887c.js"
+  },
+  {
+    "revision": "0fdd24c02d6f65488808",
+    "url": "/DragonFlyTracer/static/js/main.0fdd24c0.chunk.js"
+  },
+  {
+    "revision": "3fa6e7316f0dd91cc14d",
+    "url": "/DragonFlyTracer/static/js/2.3fa6e731.chunk.js"
+  },
+  {
+    "revision": "0fdd24c02d6f65488808",
+    "url": "/DragonFlyTracer/static/css/main.1321077e.chunk.css"
+  },
+  {
+    "revision": "4e5cb967cb5372af3590a78ba3850e70",
+    "url": "/DragonFlyTracer/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/DragonFlyTracer/precache-manifest.8ee982017dac8637a6650a4ad62c8707.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/DragonFlyTracer/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
