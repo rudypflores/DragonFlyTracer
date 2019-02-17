4f9cbda4d0790e017a83b2bd7a4eb23a6d102cf0
Rudy Flores <38470001+rudypflores@users.noreply.github.com>
2019-02-16 19:45:44 -0600

    refactor maps API

--- src/components/GoogleMaps/MapContainer.js
+++ src/components/GoogleMaps/MapContainer.js
@@ -1,55 +1,54 @@
 import React from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import './MapContainerSettings.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends React.Component {
-	state = {
-	    showingInfoWindow: false,
-	    activeMarker: {},
-	    selectedPlace: {},
-	};
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+  };
 
-  	onMarkerClick = (props, marker, e) =>
-	    this.setState({
-	      selectedPlace: props,
-	      activeMarker: marker,
-	      showingInfoWindow: true
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
     });
 
-	onMapClicked = (props) => {
-	    if (this.state.showingInfoWindow) {
-	      this.setState({
-	        showingInfoWindow: false,
-	        activeMarker: null
-	      })
-	    }
-	};
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
 
-	translateToGeocode() {
+  render() {
+    return (
+      <Map google={this.props.google}
+	    style={{width: '100%', height: '100%', position: 'relative'}}
+	    className={'map'}
+	    zoom={14}>
+		  <Marker
+		    title={'The marker`s title will appear as a tooltip.'}
+		    name={'SOMA'}
+		    position={{lat: 37.778519, lng: -122.405640}} />
+		  <Marker
+		    name={'Dolores park'}
+		    position={{lat: 37.759703, lng: -122.428093}} />
+		  <Marker />
 
-	}
-
-	//Add more elements with <marker/> tag
-  	render() {
-    	return (
-      		<Map google={window.google}
-			    style={{width: '100%', height: '60%'}}
-			    className={'map'}
-			    zoom={14}>
-			  <Marker
-			    title={'The marker`s title will appear as a tooltip.'}
-			    name={'SOMA'}
-			    position={{lat: 37.778519, lng: -122.405640}} />
-			  <Marker
-			    name={'Dolores park'}
-			    position={{lat: 37.759703, lng: -122.428093}} />
-			  <Marker />
-			  <Marker
-			    name={'New York City'}
-			    position={{lat:40.7128 , lng: -74.0060}} />
-			  <Marker />
-			</Map>
-    )
+		  	<InfoWindow
+	          marker={this.state.activeMarker}
+	          visible={this.state.showingInfoWindow}>
+	            <div>
+	              <h1>{this.state.selectedPlace.name}</h1>
+	            </div>
+	        </InfoWindow>
+		</Map>
+    );
   }
 }
 
